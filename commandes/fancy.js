d346c80ffb8cd091c7ba1d8f61ec0de29a754e91
NjabuloJ <fanajb65@gmail.com>
2025-01-23 21:18:12 +0200

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../Anyway/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "🖋️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Njabulo\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Njabulo', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 ☆ɴᴊᴀʙᴜʟᴏ-ᴊʙ✧\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('☆ɴᴊᴀʙᴜʟᴏ-ᴊʙ✧', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
