fa4fe069f74e4748a35a6ac72a7dd5e9cc17d747
NjabuloJ <fanajb65@gmail.com>
2025-01-24 09:07:02 +0200

    Update group.js

--- commandes/group.js
+++ commandes/group.js
@@ -2,22 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "channel", reaction: "🚔", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "channel", reaction: "💬", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Salut je m\'appelle *Njabulo 🥀_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
     let d = ' developpé par *Njabulo Sir💎*';
     let varmess = z + d;
-    var lien = '╭━━〔 *JOIN GROUP* 〕━━┈⊷
-┇☞ *Auto React:*  Enabled     
-┇☞ *Delete Links:* Enabled .  
-┇☞ *Anti-Link:* Enabled.         
-┇☞ *Auto Typing:* Enabled.    
-┇☞ *Always Online:* Enabled 
-┇☞ *Public Mode:* Enabled    @
-╰━━━━━━━━━━━━──┈⊷
-*_group links🖇️_* https://chat.whatsapp.com/KEG7tuWlCMs6OPEcLuNnac
-*_bot repo links🖇️_* https://github.com/NjabuloJ/Njabulo-jb
-*_power by Njabulo-JB happy ✅_*';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://chat.whatsapp.com/GfmBCCZR34g5EqMHpFLzZN';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
@@ -27,15 +17,15 @@ console.log("mon test");
 console.log("mon test");
 /*module.exports.commande = () => {
   var nomCom = ["test","t"]
-  var reaction="🚔"
+  var reaction="💬"
   return { nomCom, execute,reaction }
 };
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *Njabulo Jb* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+   let z ='Salut je m\'appelle *DULLAH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Thomas*'
       let varmess=z+d
       var img='https://telegra.ph/file/CP2BirU5pBj04cXXgEbfuv.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
-}  */ 
+}  */
