4539c891105686228bc36c30cccb0becec0b2b62
NjabuloJ <fanajb65@gmail.com>
2025-01-25 09:23:35 +0200

    Update play.js

--- commandes/play.js
+++ commandes/play.js
@@ -1,160 +1,87 @@
-require("dotenv").config();
 const {
   zokou
 } = require("../framework/zokou");
-const yts = require("yt-search");
-const BaseUrl = process.env.GITHUB_GIT;
-const giftedapikey = process.env.BOT_OWNER;
-function validateConfig() {
-  if (!BaseUrl || !giftedapikey) {
-    throw new Error("Configuration error: Missing BaseUrl or API key.");
-  }
+function delay(_0x25f170) {
+  console.log("â±ï¸ delay for " + _0x25f170 + 'ms');
+  return new Promise(_0x529a1f => setTimeout(_0x529a1f, _0x25f170));
 }
-validateConfig();
-zokou({
-  'nomCom': "video",
-  'categorie': "Search",
-  'reaction': '🎬'
-}, async (_0x246c76, _0x43608a, _0x7dc04e) => {
-  const {
-    ms: _0x5ddcc9,
-    repondre: _0x59580a,
-    arg: _0xe570ef
-  } = _0x7dc04e;
-  if (!_0xe570ef[0]) {
-    return _0x59580a("Please insert a song/video name.");
-  }
-  try {
-    const _0x78f812 = await yts(_0xe570ef.join(" "));
-    const _0x2c1653 = _0x78f812.videos;
-    if (_0x2c1653.length === 0) {
-      return _0x59580a("No videos found.");
-    }
-    const _0x459a98 = _0x2c1653[0].url;
-    const _0x5af7ff = await fetch(BaseUrl + "/api/download/ytmp4?url=" + encodeURIComponent(_0x459a98) + "&apikey=" + giftedapikey);
-    const _0x3b69f5 = await _0x5af7ff.json();
-    if (_0x3b69f5.status === 200 && _0x3b69f5.success) {
-      const _0x8564e6 = _0x3b69f5.result.download_url;
-      await _0x43608a.sendMessage(_0x246c76, {
-        'image': {
-          'url': _0x2c1653[0].thumbnail
-        },
-        'caption': "╭━━━━━═━••⊷••───⊛\n┊ 『 *NJABULO* 』\n┊ *NJABULO JB MEDIA* \n┊ *njabulo* \n╰━━━━━═━••⊷••───⊛"
-      }, {
-        'quoted': _0x5ddcc9
-      });
-      await _0x43608a.sendMessage(_0x246c76, {
-        'video': {
-          'url': _0x8564e6
-        },
-        'mimetype': "video/mp4"
-      }, {
-        'quoted': _0x5ddcc9
-      });
-      _0x59580a("*[Downloaded Successfully by Njabulo JB]✓*");
-    } else {
-      _0x59580a("Failed to download the video.");
-    }
-  } catch (_0x487250) {
-    console.error("Error:", _0x487250);
-    _0x59580a("An error occurred while processing your request.");
-  }
-});
 zokou({
   'nomCom': "play",
-  'categorie': "Download",
-  'reaction': '🎶'
-}, async (_0x5a2f72, _0x48087c, _0x242194) => {
+  'desc': "To check bot response time",
+  'Categorie': "General",
+  'reaction': '🎧',
+  'fromMe': "true"
+}, async (_0x4cfdc6, _0x41d5d3, _0x546e3d) => {
   const {
-    ms: _0x1eb161,
-    repondre: _0x3adfb5,
-    arg: _0x12a314
-  } = _0x242194;
-  if (!_0x12a314[0]) {
-    return _0x3adfb5("Please insert a song name.");
-  }
-  try {
-    const _0x230e37 = await yts(_0x12a314.join(" "));
-    const _0x215553 = _0x230e37.videos;
-    if (_0x215553.length === 0) {
-      return _0x3adfb5("No audio found.");
+    ms: _0x2c968b
+  } = _0x546e3d;
+  const _0x5a815c = Array.from({
+    'length': 0x3
+  }, () => Math.floor(Math.random() * 10000 + 1000));
+  const _0x451c3b = _0x5a815c.map(_0x526b71 => "🎶: " + _0x526b71 + " 🎶").join("\n");
+  const _0x41d2b1 = {
+    'externalAdReply': {
+      'title': "Download music",
+      'body': "play : " + _🎧,
+      'sourceUrl': "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T",
+      'thumbnailUrl': "https://files.catbox.moe/8vd9ko.jpg",
+      'mediaType': 0x1,
+      'showAdAttribution': true
     }
-    const _0x1dda6f = _0x215553[0].url;
-    const _0x15847d = await fetch(BaseUrl + "/api/download/ytmp3?url=" + encodeURIComponent(_0x1dda6f) + "&apikey=" + giftedapikey);
-    const _0x2c3cff = await _0x15847d.json();
-    if (_0x2c3cff.status === 200 && _0x2c3cff.success) {
-      const _0x5938bf = _0x2c3cff.result.download_url;
-      await _0x48087c.sendMessage(_0x5a2f72, {
-        'image': {
-          'url': _0x215553[0].thumbnail
-        },
-        'caption': "╭━━━━━═━••⊷••───⊛\n┊ 『 *Njabulo* 』\n┇ *NJABULO MD Njabulo* \n┇ *njabulo* \n╰━━━━━═━••⊷••───⊛"
-      }, {
-        'quoted': _0x1eb161
-      });
-      await _0x48087c.sendMessage(_0x5a2f72, {
-        'audio': {
-          'url': _0x5938bf
-        },
-        'mimetype': "audio/mp4"
-      }, {
-        'quoted': _0x1eb161
-      });
-      _0x3adfb5("*[Downloaded Successfully by Njabulo JB]✓*");
-    } else {
-      _0x3adfb5("Failed to download audio. Try again later.");
-    }
-  } catch (_0x2bfc0f) {
-    console.error("Error:", _0x2bfc0f);
-    _0x3adfb5("An error occurred while processing your request.");
-  }
+  };
+  const _0x54ea81 = {
+    'displayName': "NJABULO MD Support",
+    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN: NJABULO MD\nORG: NJABULO MD Nexus;\nTEL;type=CELL;type=VOICE;waid=255764182801:+255 752 593 977\nEND:VCARD"
+  };
+  await _0x41d5d3.sendMessage(_0x4cfdc6, {
+    'text': "🎬 *NJABULO JB* 🎧",
+    'contextInfo': _0x41d2b1,
+    'contacts': {
+      'displayName': "NJABULO JB Verified Contact",
+      'contacts': [_0x54ea81]
+    },
+    'quoted': _0x2c968b
+  });
+  console.log("Ping results sent successfully with contact and context info!");
 });
 zokou({
-  'nomCom': "play",
-  'categorie': "Download",
-  'reaction': '🎶'
-}, async (_0x5fbcae, _0x127b13, _0x54b141) => {
+  'nomCom': "song",
+  'desc': "To check runtime",
+  'Categorie': "General",
+  'reaction': '🎶˜',
+  'fromMe': "true"
+}, async (_0x205206, _0x161605, _0x2fdaee) => {
   const {
-    ms: _0x465547,
-    repondre: _0x3febab,
-    arg: _0x4ccb4f
-  } = _0x54b141;
-  if (!_0x4ccb4f[0]) {
-    return _0x3febab("Please insert a song name.");
-  }
-  try {
-    const _0x56a69b = await yts(_0x4ccb4f.join(" "));
-    const _0x528529 = _0x56a69b.videos;
-    if (_0x528529.length === 0) {
-      return _0x3febab("No audio found.");
+    ms: _0x4e9ae0,
+    repondre: _0x2a6ebf
+  } = _0x2fdaee;
+  const _0x766aaf = process.uptime();
+  const _0x2f3abc = new Date(_0x766aaf * 1000).toISOString().substr(11, 8);
+  const _0xae590 = {
+    'externalAdReply': {
+      'title': "NJABULO JB- MUSIC REQUEST",
+      'body': "🎶,
+      'sourceUrl': "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T",
+      'thumbnailUrl': "https://files.catbox.moe/8vd9ko.jpg",
+      'mediaType': 0x1,
+      'showAdAttribution': true
     }
-    const _0x3f250c = _0x528529[0].url;
-    const _0x27115b = await fetch(BaseUrl + "/api/download/ytmp3?url=" + encodeURIComponent(_0x3f250c) + "&apikey=" + giftedapikey);
-    const _0x1c3b78 = await _0x27115b.json();
-    if (_0x1c3b78.status === 200 && _0x1c3b78.success) {
-      const _0x1a0016 = _0x1c3b78.result.download_url;
-      await _0x127b13.sendMessage(_0x5fbcae, {
-        'image': {
-          'url': _0x528529[0].thumbnail
-        },
-        'caption': "╭━━━━━═━••⊷••───⊛\n┊ 『 *NJABULO* 』\n┊ *NJABULO JB MEDIA* \n┊ *njabulo* \n╰━━━━━═━••⊷••───⊛"
-      }, {
-        'quoted': _0x465547
-      });
-      await _0x127b13.sendMessage(_0x5fbcae, {
-        'audio': {
-          'url': _0x1a0016
-        },
-        'mimetype': "audio/mp4"
-      }, {
-        'quoted': _0x465547
-      });
-      _0x3febab("*[Downloaded Successfully by Njabulo JB]✓*");
-    } else {
-      _0x3febab("Failed to download audio. Try again later.");
-    }
-  } catch (_0x110822) {
-    console.error("Error:", _0x110822);
-    _0x3febab("An error occurred while processing your request.");
-  }
+  };
+  const _0x297b69 = {
+    'displayName': "NJABULO MD Support",
+    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN: NJABULO MD\nORG: NJABULO MD Nexus;\nTEL;type=CELL;type=VOICE;waid=255764182801:+244 752 593 977\nEND:VCARD"
+  };
+  await _0x161605.sendMessage(_0x205206, {
+    'text': "*NJABULO MD MUSIC* " + _0x2f3abc,
+    'contextInfo': _0xae590,
+    'contacts': {
+      'displayName': "DOWNLOAD SONGS Verified Contact",
+      'contacts': [_0x297b69]
+    },
+    'quoted': _0x4e9ae0
+  });
+  console.log("Uptime sent successfully with contact and context info!");
 });
+module.exports = {
+  'delay': delay
+};
